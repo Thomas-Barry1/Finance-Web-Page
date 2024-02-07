9e8f131bc96ba59320db82cf2377a1af885c0dc6
thomaslb <lagaba@live.unc.edu>
2024-02-06 20:44:28 -0500

    Ready files for Railway migration

--- db_connection.js
+++ db_connection.js
@@ -1,4 +1,6 @@
 import mysql from "mysql2";
+// Config dotnev
+require("dotenv").config();
 
 // var con = mysql.createConnection({
 //   host: "127.0.0.1",
@@ -7,9 +9,11 @@ import mysql from "mysql2";
 // });
 var pool = mysql
   .createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
   })
   .promise();
 
