f016dc9b2b566baf93f6255b17e73b45bf4bb9dd
thomaslb <lagaba@live.unc.edu>
2024-02-09 23:34:25 -0500

    Change localhost to env variable

--- db_connection.js
+++ db_connection.js
@@ -1,6 +1,6 @@
 import mysql from "mysql2";
 // Config dotnev
-import "dotenv/config";
+import "dotenv/config.js";
 
 // var con = mysql.createConnection({
 //   host: "127.0.0.1",
@@ -33,10 +33,11 @@ console.log(rows);
 
 pool = mysql
   .createPool({
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "myDb",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
   })
   .promise();
 
